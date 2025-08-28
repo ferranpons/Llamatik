c8a513e45d6818be5eff6f319c86bcebab0af826
Ferran Pons <ferranpons@gmail.com>
2025-08-28 18:11:18 +0200

    working on ios but not returning good results

--- library/src/iosMain/c_interop/include/llama_embed.h
+++ library/src/iosMain/c_interop/include/llama_embed.h
@@ -7,10 +7,24 @@
 extern "C" {
 #endif
 
-bool llama_embed_init(const char *model_path);
+// Embeddings
+bool   llama_embed_init(const char *model_path);
 float *llama_embed(const char *input_text);
-int llama_embedding_size();
-void llama_free_embedding(float *embedding);
+int    llama_embedding_size(void);
+void   llama_free_embedding(float *embedding);
+void   llama_embed_free(void);
+
+// Generation
+bool   llama_generate_init(const char *model_path);
+char  *llama_generate(const char *prompt);
+char  *llama_generate_with_context(const char *system_prompt,
+        const char *context_block,
+        const char *user_prompt);
+void   llama_generate_free(void);
+
+// Helpers / shutdown
+void   llama_free_cstr(char *p);
+void   llama_shutdown(void);
 
 #ifdef __cplusplus
 }
