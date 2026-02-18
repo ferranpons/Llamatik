e43810136adac69de74c3943163a391fa1efdc85
Ferran Pons <ferranpons@gmail.com>
2026-02-18 16:37:55 +0100

    Added RTL support and new translations

--- shared/src/commonMain/kotlin/com/llamatik/app/MainApp.kt
+++ shared/src/commonMain/kotlin/com/llamatik/app/MainApp.kt
@@ -1,13 +1,25 @@
 package com.llamatik.app
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.ScaleTransition
+import com.llamatik.app.localization.AvailableLanguages
+import com.llamatik.app.localization.getCurrentLanguage
 import com.llamatik.app.ui.screens.MainScreen
 
 @Composable
 fun MainApp() {
-    Navigator(MainScreen()) {
-        ScaleTransition(it)
+
+    val isRtl = getCurrentLanguage() == AvailableLanguages.FA
+
+    CompositionLocalProvider(
+        LocalLayoutDirection provides if (isRtl) LayoutDirection.Rtl else LayoutDirection.Ltr
+    ) {
+        Navigator(MainScreen()) {
+            ScaleTransition(it)
+        }
     }
 }
